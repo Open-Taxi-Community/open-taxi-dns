3ea28c460928a9656558db9c09c3a248ea9e085b
GoBig87 <inbody.5@gmail.com>
2024-02-19 15:22:59 -0800

    adding cmd

--- cmd/dnsapi/server/main.go
+++ cmd/dnsapi/server/main.go
@@ -1,15 +1,36 @@
 package server
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/spf13/cobra"
+	"os"
+)
+
+var (
+	projectId string
+)
 
 func init() {
 
+	globalFlags := rootCmd.PersistentFlags()
+	globalFlags.StringVar(&projectId, "project-id", os.Getenv("PROJECT_ID"), "Project ID of firestore project")
+
+	rootCmd.AddCommand()
+	rootCmd.AddCommand(run)
 }
 
 var rootCmd = &cobra.Command{
 	Use:   "server",
-	Short: "Start the DNS server",
-	Run: func(cmd *cobra.Command, args []string) {
+	Short: "Root command for the DNS API server",
+	PersistentPostRunE: func(cmd *cobra.Command, args []string) error {
+
+		return nil
+	},
+}
+
+var run = &cobra.Command{
+	Use:   "run",
+	Short: "command to start the DNS API server",
+	RunE: func(cmd *cobra.Command, args []string) error {
 
 		return nil
 	},
